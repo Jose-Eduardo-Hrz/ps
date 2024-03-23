dfa64f653e6e602783ec1a9cc1b02d73f23f2383
Jose Eduardo Hernandez <josedelacruz@azc.uam.mx>
2024-03-23 15:04:09 -0600

    Modificion en Practica 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	base: '/ps',
 	integrations: [
 		starlight({
 			title: 'PS',
+			tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 5 },
 			sidebar: [
-				{
-					label: 'Practicas',
-					autogenerate: { directory: 'practicas' },
-				},
+				{ label: 'Practicas', link: 'practicas/practica1' },
 			],
 		}),
 	],
